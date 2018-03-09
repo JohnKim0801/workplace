ccc5a30c4068ef7d631225b249b97a22722960cb
JohnKim0801 <yohanneem@naver.com>
2018-03-09 13:18:21 +0900

    ver 1.0 with 6 color

--- ColorGuessingGame.js
+++ ColorGuessingGame.js
@@ -1,65 +1,120 @@
-var colors =generateRandomColors(6);
-var h1= document.querySelector("h1");
-var resetbtn= document.querySelector("#reset")
-
+var numSquares = 6;
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
-// var squares = document.getElemnetsByClassName="square"; //다른방법
-var pickedColor =pickcolor();
-var messageDisplay = document.querySelector("#message");
 var colorDisplay = document.getElementById("colorDisplay");
-colorDisplay.textContent =pickedColor;
-resetbtn.addEventListener("click",function(){
-colors=generateRandomColors(6);
-pickedColor=pickcolor();
-colorDisplay.textContent=pickedColor;
-for(var i =0; i<squares.length;i++){
-	squares[i].style.backgroundColor=colors[i];
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons=document.querySelectorAll(".mode");
+var game={};
+
+game.init=function(){
+setupModeBtn();
+setupPickBtn();
+reset();
+};
+ game.init();
+
+function init(){
+	setupModeBtn();
+	setupPickBtn();
+    reset();
 }
-});
 
-
-for(var i =0; i<colors.length;i++){
-	squares[i].style.backgroundColor=colors[i];
-
-	squares[i].addEventListener("click",function(){
-		var clickcolor =this.style.backgroundColor;
-
-		if(clickcolor===pickedColor){
-			message.textContent="Correct!";
-			changeColor(clickcolor);
-			h1.style.backgroundColor=clickcolor;
-		}
-		else{
-			this.style.backgroundColor="#232323";
-
-			messageDisplay.textContent="Try Again!"}
+function setupModeBtn(){
+	for(var i =0; i<modeButtons.length; i++){
+		modeButtons[i].addEventListener("click",function(){
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			this.textContent==="Easy"?numSquares=3: numSquares=6;
+			reset();
 		});
-}
-
-function changeColor(color){
-	for(var i =0; i<squares.length;i++){
-		squares[i].style.backgroundColor=color;
 	}
 }
 
-function pickcolor(){  //랜덤한 칼러를 골라준다.
-	var random =Math.floor(Math.random()*colors.length);
+function setupPickBtn(){
+for(var i = 0; i < squares.length; i++){
+	// add initial colors to squares
+	squares[i].style.background = colors[i];
+	//add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked squares
+		var clickedColor = this.style.background;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor) {
+			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?";
+			changeColors(clickedColor);
+			h1.style.background = clickedColor;
+		} else {
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	});
+  }	
+}
+
+
+
+function reset(){
+	colors = generateRandomColors(numSquares);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked Color
+	colorDisplay.textContent = pickedColor;
+	resetButton.textContent="New Color"
+	
+	messageDisplay.textContent="";
+	//change colors of squares
+	for(var i = 0; i < squares.length; i++) {
+
+		if(colors[i]){
+			squares[i].style.display="block";
+			squares[i].style.background = colors[i];
+		}else{
+			squares[i].style.display="none";
+		}
+	}
+	h1.style.background = "#steelblue";
+}
+
+resetButton.addEventListener("click", function(){
+	reset();
+});
+
+function changeColors(color) {
+	//loop through all squares
+	for(var i = 0; i < squares.length; i++) {
+		//change each color to match given color
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
 }
 
-function generateRandomColors(num){
-
-	var arr =[];
-	for(var i=0; i<num; i++){
+function generateRandomColors(num) {
+	//make an array
+	var arr = [];
+	//add num random colors to arr
+	for(var i = 0; i < num; i++) {
+		//get random color and push into arr
 		arr.push(randomColor());
 	}
+	//return that array
 	return arr;
 }
 
-function randomColor(){
-	var r= Math.floor(Math.random()*256);
-	var g= Math.floor(Math.random()*256);
-	var b= Math.floor(Math.random()*256);
-
-	return "rgb("+r+", "+g+", "+b+")";
+function randomColor() {
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	//pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	//pick a "blue" from 0 - 255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+function iseven(num){
+	if(num%2==0)
+		console.log("true");
+	else
+		console.log("false");
+}
+iseven(4);
+iseven(21);
+iseven(68);
+iseven(333);
 
-var age = prompt("How old are you?");
+function factorial(num){
+	if(num === 0)
+		return 1;
+	else{
+		return num*factorial(num-1);
+	}
 
-prompt(age+" years is roughly "+age*365.0+" days");
+
+}
+console.log(factorial(5));
+console.log(factorial(2));
+console.log(factorial(10));
+factorial(0);
+
+function kebabToSnake(str){
+return str.replace(/-/i,"_");
+}
+console.log(kebabToSnake("Hello-world!"));
+kebabToSnake("dogs-are-awesome");
+kebabToSnake("blah");
